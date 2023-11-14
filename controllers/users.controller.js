161be7087221274f6c266b848740fed963d13e89
MeanBordin <bodinsak2544@gmail.com>
2023-11-14 18:51:28 +0700

    up user controller

---
+++ controllers/users.controller.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../models");
+const auth = require("../middleware/auth.middleware");
+
+
+
+module.exports  = router;
